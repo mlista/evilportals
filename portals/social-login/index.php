b9d31e55ef7c3b3ff659872ef867d48ab008dafb
Mariano Lista <mariano.lista@mercadolibre.com>
2019-08-03 19:19:08 -0300

    New social-login

---
+++ portals/social-login/index.php
@@ -0,0 +1,26 @@
+<?php
+$destination = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=394, user-scalable=no">
+<title>Social-login</title>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+<link rel="stylesheet" type="text/css" href="assets/css/social.css">
+<link rel="icon" type="image/png" href="assets/img/f8593y1ses.png"/>
+</head>
+<body>
+<a class="button button--social-login button--googleplus" href="./google/index.php"><i class="icon fa fa-google-plus"></i>Login With Google +</a>
+<br>
+<a class="button button--social-login button--twitter" href="./twitter/index.php"><i class="icon fa fa-twitter"></i>Login With Twitter</a>
+<br>
+<a class="button button--social-login button--facebook" href="./facebook/index.php"><i class="icon fa fa-facebook"></i>Login With Facebook</a>
+</body>
+</html>
